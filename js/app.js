e390dedee65a7dfe5843686612190d5c1ffe92a0
Jeffrey Baldwin <jbaldwin01@snet.net>
2018-09-14 21:45:42 -0400

    Implement timer, star rating and modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 let deck = document.querySelector(".deck");
 let restart = document.querySelector(".fa-repeat");
 let openCards = [];
+let matchCount = 0;
+let moves = 0;
+let moveString = "Moves";
+let modalSpan = document.querySelector(".modal");
+let modalContent = document.querySelector(".modal-content");
+let modalButton = document.createElement("button");
+modalButton.className = "modal-button";
+modalButton.textContent = "Yes";
+let close = document.querySelector(".close");
+let timerId = 0;
+const scorePanel = document.querySelector(".score-panel");
+let starList = document.querySelector(".stars");
+let starHTML = `<li><i class="fa fa-star"></i></li>`;
+let timerDiv = document.createElement("div");
+timerDiv.className = "timer";
+timerDiv.innerHTML = "00:00";
+scorePanel.appendChild(timerDiv);
+let totalSeconds = 0;
+let starCount = 3;
+const popupMessage = document.createElement("div");
+let firstParagraph = document.createElement("p");
+let secondParagraph = document.createElement("p");
+let thirdParagraph = document.createElement("p");
+
 /*
  * Create a list that holds all of your cards
  */
@@ -14,16 +38,19 @@ let cardList = ["fa fa-diamond", "fa fa-diamond",
                 "fa fa-bomb", "fa fa-bomb"];
 
 function initGame() {
+    matchCount = 0;
+    moves = 0;
+    starCount = 3;
+    starList.innerHTML = starHTML + starHTML + starHTML;
+    document.querySelector(".moves").innerHTML = `<span class="moves">${moves}</span> ${moveString}`;
     let shuffledCardList = shuffle(cardList);
-    //const fragment = document.createDocumentFragment();
     let htmlFrag = "";
     for(card of shuffledCardList) {
         htmlFrag += createCard(card);
-
-        //fragment.appendChild(cardHTML);
     }
 
     deck.innerHTML = htmlFrag;
+    createModal();
 }
 
 function createCard(card) {
@@ -69,7 +96,13 @@ function flipCard() {
     event.target.classList.add("open", "show");
 }
 
-function addOpenCard() {
+function closeCards() {
+    openCards[0].classList.remove("open", "show");
+    openCards[1].classList.remove("open", "show");
+    openCards = [];
+}
+
+function addToOpenCards() {
     openCards.push(event.target);
 }
 
@@ -80,25 +113,123 @@ function lockOpen() {
     openCards[1].classList.add("match");
 }
 
-function checkMatch() {
+function compareCards() {
     if (openCards[0].firstElementChild.classList[1] === openCards[1].firstElementChild.classList[1]) {
         lockOpen();
         openCards = [];
+        matchCount += 1;
     }
     else {
-        console.log("not a match");//flipCardBack();//classList.toggle("open", "show");
-        openCards[0].classList.remove("open", "show");
-        openCards[1].classList.remove("open", "show");
-        openCards = [];
+        setTimeout(closeCards, 1000);
+    }
+}
+
+function updateMoveCount() {
+    moves += 1; 
+    moveString = moves === 1 ? "Move" : "Moves";
+    document.querySelector(".moves").innerHTML = `<span class="moves">${moves}</span> ${moveString}`;
+}
+
+/*
+ * Display popup when all cards are matched.
+ */
+function createModal() {
+    firstParagraph.textContent = "Congratulations, you won!!!"
+    modalContent.appendChild(firstParagraph);
+    secondParagraph.innerHTML= "";
+    modalContent.appendChild(secondParagraph);
+    thirdParagraph.innerHTML = `Would you like to play again?`;
+    modalContent.appendChild(thirdParagraph);
+    modalContent.appendChild(modalButton);
+}
+
+function displayModal() {
+    pluralChar = starCount > 1 ? "s" : "";
+    secondParagraph.innerHTML= `Your total time was: ${timerDiv.innerHTML} and your rating was ${starCount} ${starList.firstElementChild.innerHTML}${pluralChar}`;
+    modalSpan.style.display = "block";
+}
+
+function startTimer() {
+    return setInterval(incrementTimer, 1000);
+}
+
+function incrementTimer() {
+    ++totalSeconds;
+    function addZero(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+    let min = addZero(Math.floor(totalSeconds/60));
+    let sec = addZero(totalSeconds - (min*60));
+    timerDiv.innerHTML = `${min}:${sec}`;
+}
+
+function resetTimer(){
+    clearInterval(timerId);
+    totalSeconds = 0;
+    timerDiv.innerHTML = "00:00";
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+}
+
+function decrementStars() {
+    let starElement = starList.firstElementChild;
+    if(starList.children.length > 1) { 
+        starElement.remove()
+        --starCount;
     }
 }
 
  deck.addEventListener("click", function() {
-     flipCard();
-     addOpenCard();
-     if (openCards.length > 1) {
-        checkMatch();
-     }
+    if (event.target.nodeName === 'LI') {
+        //only continue if only 1 other card is open
+        if(openCards.length <= 1) {
+            //make sure same card wasn't clicked
+            if(!event.target.classList.contains("open") && !event.target.classList.contains("show")) {
+                updateMoveCount();
+                if(moves === 1) {
+                    timerId = startTimer();
+                }
+                let starElement = starList.firstElementChild;
+                if(moves % 20 === 0 ) {
+                    decrementStars();
+                }
+                
+                flipCard();
+                addToOpenCards();
+                if (openCards.length === 2) {
+                    compareCards();
+                }
+                if(matchCount === 8) {
+                    //display modal
+                    stopTimer();
+                    matchCount = 0;
+                    displayModal();
+                }
+            }
+        }
+    }
  });
 
- restart.addEventListener("click", initGame);
+ restart.addEventListener("click", function() {
+     resetTimer(timerId);
+     initGame();
+    });
+
+close.addEventListener("click", function() {
+    modalSpan.style.display = "none";
+});
+
+modalButton.addEventListener("click", function() {
+    modalSpan.style.display = "none";
+    resetTimer(timerId);
+    initGame();
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalSpan) {
+        modalSpan.style.display = "none";
+    }
+}
